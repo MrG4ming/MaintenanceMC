702cd8f8e7425a9117fd9292b55da8ce549056a8
BuildTools <unconfigured@null.spigotmc.org>
2022-03-01 18:24:47 +0100

    Kicking players when mode is activated

--- src/de/mrg4ming/commands/MaintenanceCommand.java
+++ src/de/mrg4ming/commands/MaintenanceCommand.java
@@ -1,9 +1,11 @@
 package de.mrg4ming.commands;
 
 import de.mrg4ming.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class MaintenanceCommand implements CommandExecutor {
     @Override
@@ -11,21 +13,24 @@ public class MaintenanceCommand implements CommandExecutor {
         if(sender.isOp()) {
             if(args.length == 0) {
                 if(Main.getPlugin().maintenance) {
-                    sender.sendMessage(Main.PREFiX + "§bMaintenance mode is currently '§aon§d'.");
+                    sender.sendMessage(Main.PREFiX + "§bMaintenance mode is currently '§aon§b'.");
                 } else {
-                    sender.sendMessage(Main.PREFiX + "§bMaintenance mode is currently '§coff§d'.");
+                    sender.sendMessage(Main.PREFiX + "§bMaintenance mode is currently '§coff§b'.");
                 }
             } else if(args.length == 1) {
                 switch (args[0]) {
                     case "on" -> {
                         Main.getPlugin().maintenance = true;
+                        for(Player p : Bukkit.getServer().getOnlinePlayers()) {
+                            if(!p.isOp()) p.kickPlayer(Main.PREFiX + "§bMaintenance mode was §cactivated§b!");
+                        }
                         Main.getPlugin().saveConfig();
-                        sender.sendMessage(Main.PREFiX + "§bMaintenance mode set to '§aon§d'.");
+                        sender.sendMessage(Main.PREFiX + "§bMaintenance mode set to '§aon§b'.");
                     }
                     case "off" -> {
                         Main.getPlugin().maintenance = false;
                         Main.getPlugin().saveConfig();
-                        sender.sendMessage(Main.PREFiX + "§bMaintenance mode set to '§coff§d'.");
+                        sender.sendMessage(Main.PREFiX + "§bMaintenance mode set to '§coff§b'.");
                     }
                 }
             }
